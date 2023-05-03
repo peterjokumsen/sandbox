2db8e7f23811a5533006e592fa632876e91ea846
Peter Jokumsen <pjokumsen@gmail.com>
2023-05-03 02:36:21 +0200

    feat(apps/basic-crud): use form-factory in landing

--- ui-src/apps/basic-crud/src/app/pages/landing/landing.component.ts
+++ ui-src/apps/basic-crud/src/app/pages/landing/landing.component.ts
@@ -4,15 +4,22 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormFactoryModule, QuestionDefinition } from '@sandbox/form-factory';
 
 @Component({
   selector: 'sandbox-landing',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FormFactoryModule ],
   templateUrl: './landing.component.html',
   styleUrls: [ './landing.component.scss' ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingComponent {
+  definition: QuestionDefinition = {
+    questions: {
+      name: {},
+      email: {},
+    },
+  };
 }
