05422ff75e2379156e8a97b02de886488832740b
Peter Jokumsen <pjokumsen@gmail.com>
2023-05-04 14:15:07 +0200

    feat: create api project

---
+++ api-src/BasicCrud.Functions/CreateOrGetEntities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BasicCrud.Functions;
+
+public static class CreateOrGetEntities
+{
+    [FunctionName("CreateOrGetEntities")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "entities")] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        var requestBody = await req.ReadAsStringAsync();
+        log.LogInformation("Query: [{QueryString}], Method: [{RequestMethod}], Body: [{RequestBody}]",
+            req.Query,
+            req.Method,
+            requestBody);
+
+        return req.Method.Equals("get", StringComparison.OrdinalIgnoreCase)
+            ? new OkObjectResult(new { Message = "get all entities, to be implemented" })
+            : new CreatedResult("entities", new { Message = "create entity, to be implemented" });
+    }
+}
