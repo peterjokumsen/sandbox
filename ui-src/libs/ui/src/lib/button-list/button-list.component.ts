b9acf56a8399cde55994d096131a22fedc066db1
Peter Jokumsen <pjokumsen@gmail.com>
2023-05-15 21:49:24 +0200

    fix(ui): update ui lib to use standalone components alone

--- ui-src/libs/ui/src/lib/button-list/button-list.component.ts
+++ ui-src/libs/ui/src/lib/button-list/button-list.component.ts
@@ -14,7 +14,15 @@ import { CommonModule } from '@angular/common';
       align-items: center;
       gap: 0.5rem;
     }
+    `,
     `
+    .button-container .btn {
+      width: 200px; /* Set the width of the button or container */
+      white-space: nowrap; /* Prevent line breaks within the label */
+      overflow: hidden; /* Hide any overflow */
+      text-overflow: ellipsis; /* Display an ellipsis (...) when the label overflows */
+    }
+    `,
   ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -22,7 +30,7 @@ import { CommonModule } from '@angular/common';
 export class ButtonListComponent<T> {
   @Output() itemClick = new EventEmitter<T>();
 
-  @Input() items: T[] = [];
+  @Input() items: T[] | null = [];
   @Input() buttonType: 'warning' | 'danger' | 'primary' | 'secondary' | 'success' = 'primary';
   @Input() transformLabel: (item: T) => string = (item) => JSON.stringify(item);
 }
