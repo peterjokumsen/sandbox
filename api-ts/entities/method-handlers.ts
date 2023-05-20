7375458b33962c19754af733254110101440dc3c
Peter Jokumsen <pjokumsen@gmail.com>
2023-05-20 09:12:23 +0200

    feat(api): handle PUT to update entity (#2)

--- api-ts/entities/method-handlers.ts
+++ api-ts/entities/method-handlers.ts
@@ -4,6 +4,7 @@ import { MethodHandler } from './types';
 export const methodHandlers: Partial<Record<HTTPMethod, MethodHandler>> & { default: MethodHandler } = {
   'GET': require('./handle-get').handleGet,
   'POST': require('./handle-post').handlePost,
+  'PUT': require('./handle-put').handlePut,
   'DELETE': require('./handle-delete').handleDelete,
   'default': require('./handle-unexpected').handleUnexpected,
 };
