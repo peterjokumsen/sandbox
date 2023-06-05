384e68ab474763c7750db6a5b828835995da3f7c
Peter Jokumse <pjokumsen@gmail.com>
2023-06-05 20:59:48 +0200

    fix(api): default value for `ENV_VALUE`

--- api-ts/config/index.ts
+++ api-ts/config/index.ts
@@ -1,13 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const envValue = process.env.ENV_VALUE;
+    const envValue = process.env.ENV_VALUE ?? 'undefined';
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { envValue }
+        body: { envValue, otherVal: 'something-else' },
     };
-
 };
 
 export default httpTrigger;
